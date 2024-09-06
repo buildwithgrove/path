bbac8b7693ede8c1315da1cefd92ac3b3bda43bf
Arash <23505281+adshmh@users.noreply.github.com>
2024-09-06 14:42:40 -0400

    Add GitHub action go E2E test v0 (#9)
    This PR adds the GH workflow file plus other pieces necessary to run the
    E2E tests:
    - A script to update the E2E config file from Repo's secret(s).
    - A new `yml` file under GitHub workflows directory.
    
    ---------
    
    Signed-off-by: Arash <23505281+adshmh@users.noreply.github.com>

---
+++ e2e/scripts/update_config_from_secrets.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# This script updates the E2E config file from environment variables.
+# It is used in GitHub actions to run the CI, and the environment variables
+# are populated from repo's secrets.
+
+# Set the current working directory to e2e config directory.
+cd "$(dirname "$0")/.." || exit 1
+
+update_config_from_env() {
+    if [[ -z "$GATEWAY_PRIVATE_KEY" ]]; then
+        echo " GATEWAY_PRIVATE_KEY environment variable not set"
+        return 1
+    fi
+
+    local CONFIG_FILE="./.config.test.yaml"
+    if [[ ! -f $CONFIG_FILE ]]; then
+        echo "config file" $CONFIG_FILE "not found in" $PWD
+	return 1
+    fi
+
+    sed  -i 's/gateway_private_key:.*/gateway_private_key: '"$GATEWAY_PRIVATE_KEY"'/' $CONFIG_FILE
+}
+
+update_config_from_env "$@"
