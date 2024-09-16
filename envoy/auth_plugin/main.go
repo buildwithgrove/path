2b510310dcdf742a27475b3aef0fce75cd89bf1f
Pascal van Leeuwen <pascal@grove.city>
2024-09-16 12:21:30 +0100

    chore: add comments to filter service

--- envoy/auth_plugin/main.go
+++ envoy/auth_plugin/main.go
@@ -1,8 +1,9 @@
 //go:build auth_plugin
 
-package filter
+package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/envoyproxy/envoy/contrib/golang/common/go/api"
@@ -15,14 +16,22 @@ import (
 	"github.com/buildwithgrove/authorizer-plugin/filter"
 )
 
+// filterName is the name of the filter that Envoy will use to identify and load the plugin
+// If must match the `http_filters.typed_config.library_id` field for the Go filter in envoy.yaml
 const filterName = "authorizer-plugin"
 
+// CONFIG_PATH is set in the Envoy Docker image during the build process.
+// It points to the mounted `.config.authorizer.yaml` file. See `Dockerfile.envoy`.
+const envVarConfigPath = "CONFIG_PATH"
+
+// All configuration of the plugin must be loaded in init()
+// See https://github.com/envoyproxy/examples/blob/main/golang-http/simple/config.go#L16
 func init() {
 	logger := polyzero.NewLogger()
 
-	configPath := os.Getenv("CONFIG_PATH")
+	configPath := os.Getenv(envVarConfigPath)
 	if configPath == "" {
-		panic("CONFIG_PATH is not set in the environment")
+		panic(fmt.Sprintf("%s is not set in the environment", envVarConfigPath))
 	}
 
 	config, err := config.LoadAuthorizerPluginConfigFromYAML(configPath)
@@ -55,4 +64,6 @@ func init() {
 	envoyhttp.RegisterHttpFilterFactoryAndConfigParser(filterName, filterFactoryFunc, &filter.Parser{})
 }
 
+// func main() is only here to satisfy the Go build system
+// See https://github.com/envoyproxy/examples/blob/main/golang-http/simple/config.go#L74
 func main() {}
