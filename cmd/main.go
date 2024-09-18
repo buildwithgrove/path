214231f1abd30cee30f831851f11e572aa9069a4
Pascal van Leeuwen <pascal@grove.city>
2024-09-18 20:18:29 +0100

    chore: init helpers in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/pokt-network/poktroll/pkg/polylog/polyzero"
 
 	"github.com/buildwithgrove/path/config"
+	morseConfig "github.com/buildwithgrove/path/config/morse"
+	shannonConfig "github.com/buildwithgrove/path/config/shannon"
 	"github.com/buildwithgrove/path/gateway"
 	"github.com/buildwithgrove/path/health"
 	"github.com/buildwithgrove/path/relayer"
@@ -19,47 +21,6 @@ import (
 )
 
 const configPath = ".config.yaml"
-
-func getProtocol(config config.GatewayConfig, logger polylog.Logger) (relayer.Protocol, error) {
-
-	// Config YAML validation enforces that exactly one protocol config is set,
-	// so first check if the protocol config is set for Shannon.
-	if shannonConfig := config.GetShannonConfig(); shannonConfig != nil {
-		logger.Info().Msg("Starting PATH gateway with Shannon protocol")
-
-		fullNode, err := shannon.NewFullNode(shannonConfig.FullNodeConfig, logger)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create shannon full node: %v", err)
-		}
-
-		protocol, err := shannon.NewProtocol(context.Background(), fullNode)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create shannon protocol: %v", err)
-		}
-
-		return protocol, nil
-	}
-
-	// If the protocol config is not set for Shannon, then it must be set for Morse.
-	if morseConfig := config.GetMorseConfig(); morseConfig != nil {
-		logger.Info().Msg("Starting PATH gateway with Morse protocol")
-
-		fullNode, err := morse.NewFullNode(morseConfig.FullNodeConfig)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create morse full node: %v", err)
-		}
-
-		protocol, err := morse.NewProtocol(context.Background(), fullNode, morseConfig)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create morse protocol: %v", err)
-		}
-
-		return protocol, nil
-	}
-
-	// this should never happen but guard against it
-	return nil, fmt.Errorf("no protocol config set")
-}
 
 func main() {
 	logger := polyzero.NewLogger()
@@ -103,4 +64,50 @@ func main() {
 	if err := apiRouter.Start(); err != nil {
 		log.Fatalf("failed to start API router: %v", err)
 	}
+}
+
+/* -------------------- Gateway Init Helpers -------------------- */
+
+func getProtocol(config config.GatewayConfig, logger polylog.Logger) (relayer.Protocol, error) {
+	if shannonConfig := config.GetShannonConfig(); shannonConfig != nil {
+		return getShannonProtocol(shannonConfig, logger)
+	}
+
+	if morseConfig := config.GetMorseConfig(); morseConfig != nil {
+		return getMorseProtocol(morseConfig, logger)
+	}
+
+	return nil, fmt.Errorf("no protocol config set")
+}
+
+func getShannonProtocol(config *shannonConfig.ShannonGatewayConfig, logger polylog.Logger) (relayer.Protocol, error) {
+	logger.Info().Msg("Starting PATH gateway with Shannon protocol")
+
+	fullNode, err := shannon.NewFullNode(config.FullNodeConfig, logger)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create shannon full node: %v", err)
+	}
+
+	protocol, err := shannon.NewProtocol(context.Background(), fullNode)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create shannon protocol: %v", err)
+	}
+
+	return protocol, nil
+}
+
+func getMorseProtocol(config *morseConfig.MorseGatewayConfig, logger polylog.Logger) (relayer.Protocol, error) {
+	logger.Info().Msg("Starting PATH gateway with Morse protocol")
+
+	fullNode, err := morse.NewFullNode(config.FullNodeConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create morse full node: %v", err)
+	}
+
+	protocol, err := morse.NewProtocol(context.Background(), fullNode, config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create morse protocol: %v", err)
+	}
+
+	return protocol, nil
 }
