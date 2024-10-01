36807ded92db5d4d0dc1054e32897d87f2075885
Daniel Olshansky <olshansky.daniel@gmail.com>
2024-09-30 17:44:05 -0700

    Added mainnet archival

--- request/qos_names.go
+++ request/qos_names.go
@@ -8,11 +8,10 @@ var supportedServicesToQoSServiceName = map[relayer.ServiceID]ServiceName{
 
 	"gatewaye2e": ServiceNameEVM, // "gatewaye2e" service used in E2E tests
 
-	"0002": ServiceNameEVM, // Bitcoin MainNet
-
 	"0007": ServiceNameEVM, // Ethereum Gateway Server on POKT TestNet
 
 	"0021": ServiceNameEVM, // Ethereum Mainnet on POKT Mainnet
+	"0022": ServiceNameEVM, // Ethereum Mainnet Archival on POKT Mainnet
 	"0040": ServiceNameEVM,
 
 	"0006": ServiceNameSolana,
