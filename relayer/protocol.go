81587860a51d6d52c5576dda0fb1073855669f9f
Arash <23505281+adshmh@users.noreply.github.com>
2024-10-28 20:46:59 -0400

    Update relayer/protocol.go
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>
    Signed-off-by: Arash <23505281+adshmh@users.noreply.github.com>

--- relayer/protocol.go
+++ relayer/protocol.go
@@ -14,14 +14,16 @@ type Protocol interface {
 	health.Check
 }
 
-// ProtocolRequestContext defines the functionality expected by the gateway
-// from the protocol, once a request context has been established for a particular service ID.
-// These include:
-// 1. List the endpoins available for sending relays for a specific service.
-// 2. Send a relay to a specific endpoint and return its response.
-// There are two implementations of this interface:
-// - Morse: in the relayer/morse package, and
-// - Shannon: in the relayer/shannon package.
+// ProtocolRequestContext defines the functionality expected by the gateway from the protocol,
+// for a particular service ID.
+//
+// These include but not limited to:
+// 	1. Listing the endpoins available for sending relays for a specific service.
+// 	2. Send a relay to a specific endpoint and return its response.
+// 
+// The first two implementations of this interface are (as of writing) are:
+// 	- Morse: in the relayer/morse package, and
+// 	- Shannon: in the relayer/shannon package.
 type ProtocolRequestContext interface {
 	// TODO_TECHDEBT: any protocol/network-level errors should result in
 	// the endpoint being dropped by the protocol instance from the returned
