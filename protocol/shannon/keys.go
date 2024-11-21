abfe34a7a41d2d29e73f0ea50a49a69f8da597c1
Arash <23505281+adshmh@users.noreply.github.com>
2024-11-21 14:08:33 -0500

    Update protocol/shannon/keys.go
    Co-authored-by: Redouane Lakrache <r3d0ne@gmail.com>
    Signed-off-by: Arash <23505281+adshmh@users.noreply.github.com>

--- protocol/shannon/keys.go
+++ protocol/shannon/keys.go
@@ -7,7 +7,7 @@ import (
 )
 
 // getSec256k1PrivateKeyFromKeyHex returns a Sec256k1 private key from the supplied hex-encoded private key string.
-// It allows any configuration-related code to build sec256k1 private keys from hex-encoded private keys.
+// It allows any configuration-related code to build secp256k1 private keys from hex-encoded private keys.
 func getSec256k1PrivateKeyFromKeyHex(privateKeyHex string) (*secp256k1.PrivKey, error) {
 	privateKeyBz, err := hex.DecodeString(privateKeyHex)
 	if err != nil {
