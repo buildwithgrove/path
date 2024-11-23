a794ef0638fda784c8e14011cc2be936afbebc4f
Arash Deshmeh <adshmh@gmail.com>
2024-11-22 21:30:00 -0500

    Address review comment: update protocol.Payload godoc

--- protocol/payload.go
+++ protocol/payload.go
@@ -1,8 +1,9 @@
 package protocol
 
-// TODO_TECHDEBT(@adshmh): use an interface here that returns the serialized form of the request.
-// Payload should return the serialized form of the request to be delivered to the backend service,
+// TODO_TECHDEBT(@adshmh): use an interface here that returns the serialized form of the request, with the following requirements:
+// 1. Payload should return the serialized form of the request to be delivered to the backend service,
 // i.e. the onchain service to which the protocol endpoint proxies relay requests.
+// 2. Use an enum to represent the underlying spec/standard, e.g. REST/JSONRPC/gRPC/etc.
 //
 // Payload currently only supports HTTP(s) requests to an EVM blockchain (through its Data/Method/Path fields)
 // TODO_DOCUMENT(@adshmh): add more examples, e.g. for RESTful services, as support for more types of services
