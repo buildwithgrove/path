abb96c3942af3a1f9d7223d2ba2ae1ce8c3e958d
Arash Deshmeh <adshmh@gmail.com>
2024-11-26 10:15:42 -0500

    Make initial changes to EVM using proto metrics

--- qos/evm/evm.go
+++ qos/evm/evm.go
@@ -1,9 +1,17 @@
 package evm
 
 import (
+	"encoding/json"
+	"errors"
+	"context"
+	"io"
+	"net/http"
+
 	"github.com/pokt-network/poktroll/pkg/polylog"
 
 	"github.com/buildwithgrove/path/gateway"
+	qosobservations "github.com/buildwithgrove/path/observation/qos"
+	"github.com/buildwithgrove/path/qos/jsonrpc"
 )
 
 // QoS struct performs the functionality defined by gateway package's ServiceQoS,
@@ -17,13 +25,47 @@ var _ gateway.QoSService = &QoS{}
 // It contains logic specific to EVM-based chains, including request parsing,
 // response building, and endpoint validation/selection.
 type QoS struct {
-	endpointStore *EndpointStore
-	logger        polylog.Logger
+	*EndpointStore
+	ServiceState *ServiceState
+	Logger polylog.Logger
 }
 
-func NewServiceQoS(endpointStore *EndpointStore, logger polylog.Logger) *QoS {
-	return &QoS{
-		endpointStore: endpointStore,
-		logger:        logger,
+// ParseHTTPRequest builds a request context from the provided HTTP request.
+// It returns an error if the HTTP request cannot be parsed as a JSONRPC request.
+//
+// This method implements the gateway.QoSService interface.
+func (qos *QoS) ParseHTTPRequest(_ context.Context, req *http.Request) (gateway.RequestQoSContext, bool) {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return requestContextFromInternalError(err), false
 	}
+
+	var jsonrpcReq jsonrpc.Request
+	if err := json.Unmarshal(body, &jsonrpcReq); err != nil {
+		return requestContextFromUserError(err), false
+	}
+
+	// TODO_TECHDEBT(@adshmh): validate the resulting JSONRPC request to block invalid requests from being sent to endpoints.
+	// TODO_IMPROVE(@adshmh): method-specific validation of the JSONRPC request.
+	return &requestContext{
+		JSONRPCReq:    jsonrpcReq,
+		EndpointStore: qos.EndpointStore,
+		Logger:        qos.Logger,
+
+		isValid: true,
+	}, true
+}
+
+// ApplyObservations updates the stored endpoints and the "estimated" blockchain state using the supplied observations.
+// This method implements the gateway.QoSService interface.
+func (q *QoS) ApplyObservations(observations qosobservations.QoSDetails) error {
+	evmObservations := observations.EvmDetails
+	if emvObservations == nil {
+		return errors.New("ApplyObservations: received nil EVM observation")
+	}
+
+	updatedEndpoints := q.EndpointStore.UpdateEndpointsFromObservations(evmObservations)
+
+	// update the (estimated) current state of the blockchain.
+	return q.ServiceState.UpdateFromEndpoints(updatedEndpoints)
 }
---
+++ qos/evm/state.go
@@ -0,0 +1,83 @@
+package solana
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/pokt-network/poktroll/pkg/polylog"
+
+	"github.com/buildwithgrove/path/protocol"
+)
+
+// SerciceState keeps the expected current state of the solana blockchain based on the endpoints' responses to
+// different requests.
+type ServiceState struct {
+	Logger polylog.Logger
+
+	stateLock sync.RWMutex
+	// estimatedEpoch is the estimated current epoch based on endpoints' responses to `getEpochInfo` requests.
+	// See the following link for more details:
+	// https://solana.com/docs/rpc/http/getepochinfo
+	estimatedEpoch uint64
+	// estimatedBlockHeight is the estimated blockheight based on endpoints' responses to `getEpochInfo` requests.
+	estimatedBlockHeight uint64
+}
+
+// TODO_FUTURE: add an endpoint ranking method which can be used to assign a rank/score to a valid endpoint to guide endpoint selection.
+//
+// ValidateEndpoint returns an error if the supplied endpoint is not valid based on the estimated state of Solana blockchain.
+func (s *ServiceState) ValidateEndpoint(endpoint endpoint) error {
+	s.stateLock.RLock()
+	defer s.stateLock.RUnlock()
+
+	if err := endpoint.ValidateBasic(); err != nil {
+		return err
+	}
+
+	if endpoint.GetEpochInfoResult.Epoch < s.estimatedEpoch {
+		return fmt.Errorf("endpoint has epoch %d, estimated current epoch is %d", endpoint.GetEpochInfoResult.Epoch, s.estimatedEpoch)
+	}
+
+	if endpoint.GetEpochInfoResult.BlockHeight < s.estimatedBlockHeight {
+		return fmt.Errorf("endpoint has block height %d, estimated block height is %d", endpoint.GetEpochInfoResult.BlockHeight, s.estimatedBlockHeight)
+	}
+
+	return nil
+}
+
+// UpdateFromObservations updates the service state using estimation(s) deriven from the set of updated endpoints, i.e. the set of endpoints for which
+// an observation was received.
+func (s *ServiceState) UpdateFromEndpoints(updatedEndpoints map[protocol.EndpointAddr]endpoint) error {
+	s.stateLock.Lock()
+	defer s.stateLock.Unlock()
+
+	for endpointAddr, endpoint := range updatedEndpoints {
+		if err := endpoint.ValidateBasic(); err != nil {
+			continue
+		}
+
+		// The endpoint's Epoch should be at-least equal to the estimated epoch before being used to update the estimated state of Solana blockchain.
+		if endpoint.GetEpochInfoResult.Epoch < s.estimatedEpoch {
+			continue
+		}
+
+		// The endpoint's BlockHeight should be greater than the estimated block height before being used to update the estimated state of Solana blockchain.
+		if endpoint.GetEpochInfoResult.BlockHeight <= s.estimatedBlockHeight {
+			continue
+		}
+
+		// TODO_TECHDEBT: use a more resilient method for updating block height.
+		// e.g. one endpoint returning a very large number as block height should
+		// not result in all other endpoints being marked as invalid.
+		s.estimatedEpoch = endpoint.GetEpochInfoResult.Epoch
+		s.estimatedBlockHeight = endpoint.GetEpochInfoResult.BlockHeight
+
+		s.Logger.With(
+			"endpoint", endpointAddr,
+			"block height", s.estimatedBlockHeight,
+			"epoch", s.estimatedEpoch,
+		).Info().Msg("Updating latest block height")
+	}
+
+	return nil
+}
