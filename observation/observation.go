a69148440a78dcd3e0ab38cae67444c6cd1caaa0
Arash <23505281+adshmh@users.noreply.github.com>
2024-12-19 18:44:07 -0500

    Update observation/observation.go
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>

--- observation/observation.go
+++ observation/observation.go
@@ -1,5 +1,5 @@
 // package observation defines all the structures used to communicate all aspects of an observation by each component of PATH.
-// This includes, but is not limited to the details of:
+// This includes, but is not limited to observations of:
 // 1. HTTP service requests
 // 2. Protocol-level observations: e.g. latency of an endpoint.
 // 3. QoS-level observations: e.g. Solana blockchain Epoch reported by an endpoint.
