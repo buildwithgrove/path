2f191639b783eeb651c20ee872ba7d70d23b6fb0
Arash <23505281+adshmh@users.noreply.github.com>
2024-12-19 18:43:32 -0500

    Update gateway/observe.go
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>

--- gateway/observe.go
+++ gateway/observe.go
@@ -4,11 +4,11 @@ import (
 	"github.com/buildwithgrove/path/observation"
 )
 
-// RequestResponseReporter defines the interface for reporting all the details
-// regarding a request and its corresponding response and set of events to any interested entity.
-// Examples of reporters includes:
-// - The MetricsReporter component: to export metrics based on the observations.
-// - The DataReporter: to export the observations to an external component: eg. a Messaging system or a Database.
+// RequestResponseReporter defines the interface for reporting observations with respect to
+// a request, its corresponding response, and the set of events to any interested entity.
+// Examples of reporters include:
+// 	- MetricsReporter: exports metrics based on the observations
+// 	- DataReporter: exports observations to external components (e.g.Messaging system or Database)
 type RequestResponseReporter interface {
 	// Publish exports the details of the service request and response(s) to the external component used by the corresponding implementation.
 	Publish(observation.RequestResponseDetails)
