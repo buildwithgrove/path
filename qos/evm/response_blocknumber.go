3e0a855594f4467a86ef67aba01ff96e20154b6c
Arash <23505281+adshmh@users.noreply.github.com>
2025-01-16 11:03:34 -0500

    Update qos/evm/response_blocknumber.go
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>

--- qos/evm/response_blocknumber.go
+++ qos/evm/response_blocknumber.go
@@ -16,7 +16,8 @@ var _ response = responseToBlockNumber{}
 // into a responseToBlockNumber struct, adding any encountered errors
 // to the returned struct.
 func responseUnmarshallerBlockNumber(jsonrpcReq jsonrpc.Request, jsonrpcResp jsonrpc.Response, logger polylog.Logger) (response, error) {
-	if jsonrpcResp.Error.Code != 0 { // The endpoint returned an error: no need to do further processing of the response.
+	// The endpoint returned an error: no need to do further processing of the response.
+	if jsonrpcResp.Error.Code != 0 {
 		// Note: this assumes the `eth_blockNumber` request sent to the endpoint was valid.
 		return responseToBlockNumber{
 			Response: jsonrpcResp,
