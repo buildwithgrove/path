273b690fec9b06a91f45758203826a6265fc26e4
Arash <23505281+adshmh@users.noreply.github.com>
2025-02-21 12:01:48 -0500

    [QoS] Support array of objects as JSONRPC params (#170)
    ## Summary
    
    Support an array of objects as `params` field value in JSONRPC requests.
    
    Changes:
    
    - Add a new `Params` struct under `qos/jsonrpc` package
    - Add unit tests to cover different formats of the `params` field in a
    JSONRPC request.
    - Add a TODO for looking into batch JSONRPC requests.
    
    ## Issue
    
    - Description: Request parsing fails for raw payloads representing
    JSONRPC requests with `params` field value as an array of objects.
    
    ## Type of change
    
    Select one or more from the following:
    
    - [x] New feature, functionality or library
    - [ ] Bug fix
    - [ ] Code health or cleanup
    - [ ] Documentation
    - [ ] Other (specify)
    
    ## QoS Checklist
    
    - [ ] 1. `make path_up` or `path_up_standalone`
    - [ ] 2. Run one of the following:
    - For `path_up_standalone` with `anvil` on `Shannon`: `make
    test_request__relay_util_1000`
    - For `path_up` with `anvil` on `Shannon`: `make
    test_request__envoy_relay_util_100`
    - For `path_up` with `F00C` on `Morse`: `make
    test_request__relay_util_100_F00C_via_envoy`
    - [ ] 3. Visit [PATH Relay Grafana
    Dashboard](http://localhost:3000/d/relays/path-service-requests) to view
    results
    
    ## Sanity Checklist
    
    - [x] I have updated the GitHub Issue `assignees`, `reviewers`,
    `labels`, `project`, `iteration` and `milestone`
    - [ ] For docs, I have run `make docusaurus_start`
    - [ ] For code, I have run `make test_all`
    - [ ] For configurations, I have update the documentation
    - [x] I added TODOs where applicable

--- qos/jsonrpc/request.go
+++ qos/jsonrpc/request.go
@@ -16,12 +16,5 @@ type Request struct {
 	ID      ID      `json:"id,omitempty"`
 	JSONRPC Version `json:"jsonrpc"`
 	Method  Method  `json:"method"`
-	// TODO_MVP(@adshmh): support other forms of params field, based on the JSONRPC spec.
-	// See the link below for more details:
-	// https://www.jsonrpc.org/specification
-	//
-	// For an example of a JSONRPC request where the params field
-	// is not a slice of strings, see the link below:
-	// https://ethereum.org/en/developers/docs/apis/json-rpc/#eth_signtransaction
-	Params []string `json:"params,omitempty"`
+	Params  Params  `json:"params,omitempty"`
 }
