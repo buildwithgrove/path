0c7ef939281ec8389cbd252c6488988b2fe65d5b
Arash Deshmeh <adshmh@gmail.com>
2025-02-26 10:03:10 -0500

    Export Prometheus metrics on Morse observations

---
+++ metrics/protocol/protocol.go
@@ -0,0 +1,20 @@
+// Package qos handles exporting of all qos-related metrics.
+package protocol
+
+import (
+	"github.com/buildwithgrove/path/metrics/protocol/morse"
+	"github.com/buildwithgrove/path/observation/protocol"
+)
+
+// PublishMetrics builds and exports all protocol-related metrics using protocol-level observations.
+func PublishMetrics(protocolObservations *protocol.Observations) {
+	if protocolObservations == nil {
+		return
+	}
+
+	// Publish Morse metrics.
+	if morseObservations := protocolObservations.GetMorse(); morseObservations != nil {
+		morse.PublishMetrics(morseObservations)
+	}
+	// TODO_MVP(@adshmh): add calls to metric exporter functions for the Shannon protocol.
+}
