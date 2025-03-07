09000bfcc1b2294ae3fd92797974d44eee9411a9
Pascal van Leeuwen <pascal@grove.city>
2025-03-07 14:25:59 +0000

    fix: implement review comments

--- makefiles/guard.mk
+++ makefiles/guard.mk
@@ -5,8 +5,8 @@
 .PHONY: copy_local_config
 copy_local_config: copy_path_config copy_guard_config ## Runs copy_path_config and copy_guard_config
 
-.PHONY: copy_path_config
-copy_path_config: ## Copies the Shannon PATH configuration file to the local directory.
+.PHONY: copy_shannon_path_config
+copy_shannon_path_config: ## Copies the Shannon PATH configuration file to the local directory.
 	@if [ ! -f ./local/path/.config.yaml ]; then \
 		cp ./config/examples/config.shannon_example.yaml ./local/path/.config.yaml; \
 		echo "################################################################"; \
@@ -29,7 +29,44 @@ copy_path_config: ## Copies the Shannon PATH configuration file to the local dir
 		echo "################################################################"; \
 	fi
 	@if [ ! -f ./local/path/.values.yaml ]; then \
-		cp ./local/path/values.template.yaml ./local/path/.values.yaml; \
+		cp ./local/path/values.tmpl.yaml ./local/path/.values.yaml; \
+		echo "################################################################"; \
+		echo "Created ./local/path/.values.yaml"; \
+		echo "################################################################"; \
+	else \
+		echo "################################################################"; \
+		echo "Warning: ./local/path/.values.yaml already exists"; \
+		echo "To recreate the file, delete it first and run this command again"; \
+		echo "	rm ./local/path/.values.yaml"; \
+		echo "	make copy_path_config"; \
+		echo "################################################################"; \
+	fi
+
+.PHONY: copy_morse_path_config
+copy_morse_path_config: ## Copies the Morse PATH configuration file to the local directory.
+	@if [ ! -f ./local/path/.config.yaml ]; then \
+		cp ./config/examples/config.morse_example.yaml ./local/path/.config.yaml; \
+		echo "################################################################"; \
+		echo "Created ./local/path/.config.yaml"; \
+		echo ""; \
+		echo "Next steps:"; \
+		echo ""; \
+		echo "For external contributors:"; \
+		echo "  Update the following values in .morse.config.yaml:"; \
+		echo "    - relay_signing_key"; \
+		echo "    - signed_aats"; \
+		echo "################################################################"; \
+		echo "################################################################"; \
+	else \
+		echo "################################################################"; \
+		echo "Warning: ./local/path/.config.yaml already exists"; \
+		echo "To recreate the file, delete it first and run this command again"; \
+		echo "	rm ./local/path/.config.yaml"; \
+		echo "	make copy_path_config"; \
+		echo "################################################################"; \
+	fi
+	@if [ ! -f ./local/path/.values.yaml ]; then \
+		cp ./local/path/values.tmpl.yaml ./local/path/.values.yaml; \
 		echo "################################################################"; \
 		echo "Created ./local/path/.values.yaml"; \
 		echo "################################################################"; \
@@ -43,9 +80,9 @@ copy_path_config: ## Copies the Shannon PATH configuration file to the local dir
 	fi
 
 .PHONY: copy_guard_config
-copy_guard_config: ## Substitutes the sensitive 0Auth environment variables in the template envoy configuration yaml file and outputs the result to .envoy.yaml
+copy_guard_config: ## Copies the GUARD values template file to the local directory.
 	@if [ ! -f ./local/guard/.values.yaml ]; then \
-		cp ./local/guard/values.template.yaml ./local/guard/.values.yaml; \
+		cp ./local/guard/values.tmpl.yaml ./local/guard/.values.yaml; \
 		echo "################################################################"; \
 		echo "Created ./local/guard/.values.yaml"; \
 		echo "################################################################"; \
