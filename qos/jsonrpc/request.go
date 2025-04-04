93b02b2d9d54b761bfd00fc68912a9cdee6ac8be
Pascal van Leeuwen <pascalvanl@proton.me>
2025-04-04 13:56:11 +0000

    chore: rename jsonrpc.BuildParamsFromStringArray

--- qos/jsonrpc/request.go
+++ qos/jsonrpc/request.go
@@ -24,29 +24,6 @@ type Request struct {
 	Params  Params  `json:"params,omitempty"`
 }
 
-// BuildParamsFromStrings builds a Params object from an array of strings.
-//
-// For example, for an `eth_getBalance` request, the params would look like:
-// params - ["0x28C6c06298d514Db089934071355E5743bf21d60", "0xe71e1d"]]
-//
-// JSON-RPC array params must be passed in the order specified by the method.
-// Reference: https://www.jsonrpc.org/specification#parameter_structures
-//
-// TODO_FUTURE(@commoddity): other helper methods may be required to build
-// params for different JSON-RPC methods, eg. ["<string>", <bool>]
-func BuildArrayParamsFromStrings(params [2]string) (Params, error) {
-	for i, param := range params {
-		if param == "" {
-			return Params{}, fmt.Errorf("param at index %d is empty", i)
-		}
-	}
-	jsonParams, err := json.Marshal(params)
-	if err != nil {
-		return Params{}, err
-	}
-	return Params{rawMessage: jsonParams}, nil
-}
-
 // MarshalJSON customizes the JSON serialization of a Request.
 // It returns a serialized version of the receiver with empty fields (e.g. ID, Params, etc) omitted
 func (r Request) MarshalJSON() ([]byte, error) {
@@ -74,4 +51,27 @@ func (r Request) MarshalJSON() ([]byte, error) {
 
 	// Marshal and return the serializable version of the request
 	return json.Marshal(out)
+}
+
+// BuildParamsFromStrings builds a Params object from an array of strings.
+//
+// For example, for an `eth_getBalance` request, the params would look like:
+// params - ["0x28C6c06298d514Db089934071355E5743bf21d60", "0xe71e1d"]]
+//
+// JSON-RPC array params must be passed in the order specified by the method.
+// Reference: https://www.jsonrpc.org/specification#parameter_structures
+//
+// TODO_FUTURE(@commoddity): other helper methods may be required to build
+// params for different JSON-RPC methods, eg. ["<string>", <bool>]
+func BuildParamsFromStringArray(params [2]string) (Params, error) {
+	for i, param := range params {
+		if param == "" {
+			return Params{}, fmt.Errorf("param at index %d is empty", i)
+		}
+	}
+	jsonParams, err := json.Marshal(params)
+	if err != nil {
+		return Params{}, err
+	}
+	return Params{rawMessage: jsonParams}, nil
 }
