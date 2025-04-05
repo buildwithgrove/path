65c980014f9242ac2cff4b3bdd3de55f1a9970d9
Pascal van Leeuwen <pascalvanl@proton.me>
2025-04-05 09:02:21 +0000

    evm sync allowance

--- qos/evm/service_qos_config.go
+++ qos/evm/service_qos_config.go
@@ -10,6 +10,11 @@ const QoSType = "evm"
 // practices for defining what constitutes an archival block.
 const DefaultEVMArchivalThreshold = 128
 
+// defaultEVMBlockNumberSyncAllowance is the default sync allowance for EVM-based chains.
+// This number indicates how many blocks behind the perceived
+// block number the endpoint may be and still be considered valid.
+const defaultEVMBlockNumberSyncAllowance = 5
+
 // ServiceQoSConfig defines the base interface for service QoS configurations.
 // This avoids circular dependency with the config package.
 type ServiceQoSConfig interface {
