bad5df34b89d83830d759271f3d7d20baf4c5525
Pascal van Leeuwen <pascal@grove.city>
2025-06-13 11:25:07 +0200

    implement specific error handling for observations for delegated mode

--- protocol/shannon/errors.go
+++ protocol/shannon/errors.go
@@ -20,21 +20,7 @@ var (
 	//
 	// Unsupported gateway mode
 	errProtocolContextSetupUnsupportedGatewayMode = errors.New("unsupported gateway mode")
-	// Centralized gateway mode: Error getting onchain data for app
-	errProtocolContextSetupCentralizedAppFetchErr = errors.New("error getting onchain data for app owned by the gateway")
-	// Centralized gateway mode app does not delegate to the gateway.
-	errProtocolContextSetupCentralizedAppDelegation = errors.New("centralized gateway mode app does not delegate to the gateway")
-	// Centralized gateway mode: no active sessions could be retrieved for the service.
-	errProtocolContextSetupCentralizedNoSessions = errors.New("no active sessions could be retrieved for the service")
 
-	// Delegated gateway mode: could not extract app from HTTP request.
-	errProtocolContextSetupGetAppFromHTTPReq = errors.New("error getting the selected app from the HTTP request")
-	// Delegated gateway mode: could not fetch session for app from the full node
-	errProtocolContextSetupFetchSession = errors.New("error getting a session from the full node for app")
-	// Delegated gateway mode: gateway does not have delegation for the app.
-	errProtocolContextSetupAppDoesNotDelegate = errors.New("gateway does not have delegation for app")
-	// Delegated gateway mode: app is not staked for the service.
-	errProtocolContextSetupAppNotStaked = errors.New("app is not staked for the service")
 	// No endpoints available for the service.
 	// Can be due to one or more of the following:
 	// - Any of the gateway mode errors above.
