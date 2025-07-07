a13fe0de6cb449eae8e387a406b83b862063aa45
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-07-06 19:31:44 -0700

    WIP

--- protocol/shannon/protocol.go
+++ protocol/shannon/protocol.go
@@ -6,7 +6,10 @@ import (
 	"net/http"
 
 	"github.com/pokt-network/poktroll/pkg/polylog"
+	apptypes "github.com/pokt-network/poktroll/x/application/types"
+	servicetypes "github.com/pokt-network/poktroll/x/service/types"
 	sessiontypes "github.com/pokt-network/poktroll/x/session/types"
+	sdk "github.com/pokt-network/shannon-sdk"
 
 	"github.com/buildwithgrove/path/gateway"
 	"github.com/buildwithgrove/path/health"
@@ -29,6 +32,35 @@ var (
 // devtools.ProtocolDisqualifiedEndpointsReporter is fulfilled by the Protocol struct below.
 // This allows the protocol to report its sanctioned endpoints data to the devtools.DisqualifiedEndpointReporter.
 var _ devtools.ProtocolDisqualifiedEndpointsReporter = &Protocol{}
+
+// FullNode defines the set of capabilities the Shannon protocol integration needs
+// from a fullnode for sending relays.
+//
+// A properly initialized fullNode struct can:
+// 1. Return the onchain apps matching a service ID.
+// 2. Fetch a session for a (service,app) combination.
+// 3. Validate a relay response.
+// 4. Etc...
+type FullNode interface {
+	// GetApp returns the onchain application matching the application address
+	GetApp(ctx context.Context, appAddr string) (*apptypes.Application, error)
+
+	// GetSession returns the latest session matching the supplied service+app combination.
+	// Sessions are solely used for sending relays, and therefore only the latest session for any service+app combination is needed.
+	// Note: Shannon returns the latest session for a service+app combination if no blockHeight is provided.
+	GetSession(ctx context.Context, serviceID protocol.ServiceID, appAddr string) (sessiontypes.Session, error)
+
+	// ValidateRelayResponse validates the raw bytes returned from an endpoint (in response to a relay request) and returns the parsed response.
+	ValidateRelayResponse(supplierAddr sdk.SupplierAddress, responseBz []byte) (*servicetypes.RelayResponse, error)
+
+	// IsHealthy returns true if the FullNode instance is healthy.
+	// A LazyFullNode will always return true.
+	// A CachingFullNode will return true if it has data in app and session caches.
+	IsHealthy() bool
+
+	// GetAccountClient returns the account client from the fullnode, to be used in building relay request signers.
+	GetAccountClient() *sdk.AccountClient
+}
 
 // NewProtocol instantiates an instance of the Shannon protocol integration.
 func NewProtocol(
@@ -213,7 +245,6 @@ func (p *Protocol) BuildRequestContextForEndpoint(
 	// Return new request context for the pre-selected endpoint
 	return &requestContext{
 		logger:             p.logger,
-		context:            ctx,
 		fullNode:           p.FullNode,
 		selectedEndpoint:   &selectedEndpoint,
 		serviceID:          serviceID,
