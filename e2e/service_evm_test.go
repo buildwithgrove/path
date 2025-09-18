1bf5a0c6b989d817d7b17808c3c9904afd61f6cb
Pascal van Leeuwen <pascal@grove.city>
2025-09-18 15:38:42 +0100

    fix: implement review comments

--- e2e/service_evm_test.go
+++ e2e/service_evm_test.go
@@ -86,6 +86,12 @@ func getEVMTestMethodsForWebSocket() []string {
 		eth_getBalance,
 		eth_getBlockByNumber,
 		eth_getTransactionCount,
+		// TODO_INVESTIGATE(@commoddity): eth_getTransactionReceipt consistently fails on websocket
+		// tests for eth only. Disabled to allow our overall E2E websocket tests to work.
+		// However, we should investigate why this is happening.
+		// Example error:
+		// {"jsonrpc":"2.0","id":1,"error":{"code":-32000,"message":"getReceipt error: seekInFiles(invIndex=unknown index,txNum=2786993528) but data before txNum=2900000000 not available"}
+		// eth_getTransactionReceipt,
 		eth_getTransactionByHash,
 		eth_call,
 	}
