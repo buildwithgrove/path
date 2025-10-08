e781b4536bfabca405a06535a641324a3f07a1b3
Pascal van Leeuwen <pascal@grove.city>
2025-10-08 18:37:47 +0100

    generate latest sdks

--- portal-db/sdk/typescript/src/models/index.ts
+++ portal-db/sdk/typescript/src/models/index.ts
@@ -1,15 +1,4 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './Networks';
-export * from './Organizations';
-export * from './PortalAccountRbac';
-export * from './PortalAccounts';
-export * from './PortalApplicationRbac';
-export * from './PortalApplications';
-export * from './PortalPlans';
-export * from './PortalWorkersAccountData';
 export * from './RpcArmorPostRequest';
 export * from './RpcGenSaltPostRequest';
-export * from './ServiceEndpoints';
-export * from './ServiceFallbacks';
-export * from './Services';
